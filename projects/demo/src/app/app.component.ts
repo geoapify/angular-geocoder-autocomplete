bef0ed56626362d3f07a7e1b87c404391fbc4102
Oleg Hovsepyan <hovsepyan021@gmail.com>
2025-08-11 13:44:58 +0400

    GD-94: Maintain Geocoder-Autocomplete (+ Angular + React) libraries
    *Upgrade Angular 18 -> 20

--- projects/demo/src/app/app.component.ts
+++ projects/demo/src/app/app.component.ts
@@ -2,9 +2,10 @@ import { Component } from '@angular/core';
 import { CountyCode } from '@geoapify/geocoder-autocomplete';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css'],
+    standalone: false
 })
 export class AppComponent {
   displayValue = '';
